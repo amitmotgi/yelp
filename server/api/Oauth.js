5b7b5184500ab4b23b1f3edcced15ed19ee5a9d7
Amit Motgi <amit.motgi@gmail.com>
2017-03-25 20:21:06 -0700

    Update Oauth.js

--- server/api/Oauth.js
+++ server/api/Oauth.js
@@ -1,8 +1,8 @@
 // OAUTH 1.0 - Currently supporting
-export const oauth_consumer_key = "Mmo-MJx_FR6JnzrL9ac6Ew";
-export const oauth_signature = "szFCnVMReNLnYs76uKs5XIEaeuU";
+export const oauth_consumer_key = "";
+export const oauth_signature = "";
 
-export const oauth_token = "jFrj-ZhPe8srTn00UxXsWlqdLJJ0X1rp";
-export const oauth_token_secret = "8juLR1zbPxOFE6D_k20qqKm6JfY";
+export const oauth_token = "";
+export const oauth_token_secret = "";
 
 export const oauth_signature_method = "hmac-sha1";
